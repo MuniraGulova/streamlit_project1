51920cdd556ddbbf024ec53dc6a25a49846cd0ec
Munira <144519028+MuniraGulova@users.noreply.github.com>
2025-02-17 18:50:08 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 App Name')
 
 st.write('Welcome 🌸🌸🌸🌸 !')
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander('Data'):
+    st.write('X')
+    X_row as = df.drop("spicies", exis=1)
+    st.dataframe(X_row)
+
+    st.write("y")
+    y_row=df.species
+    st.dataframe(X_row)
