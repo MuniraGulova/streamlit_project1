799611bcc4c0a49954db63085c68fcbb0a372c71
Munira <144519028+MuniraGulova@users.noreply.github.com>
2025-02-17 19:05:44 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ with st.expander('Data'):
 
 with st.sidebar:
     st.header("Введите признаки: ")
-    island=st.selectbar('island', ('forgersen', 'Dream', 'Biscoe'))
+    island=st.selectbox('island', ('forgersen', 'Dream', 'Biscoe'))
     bill_length_mm = st.slider('Bill length (mm)', 32.1, 999.6, 44.5)
     bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.3)
     flipper_length_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44.5)
