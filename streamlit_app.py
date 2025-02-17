6dd03952424d1361b6a7192c2874d4439ec31e6d
Munira <144519028+MuniraGulova@users.noreply.github.com>
2025-02-17 19:45:51 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ data = {
 }
 
 input_df = pd.DataFrame(data, index=[0])
-input_penguins = pd.concat([input_df, X_raw], axis=0)
+input_penguins = pd.concat([input_df, X_row], axis=0)
 
 with st.expander('Input features'):
     st.write('**Input plugins**')
